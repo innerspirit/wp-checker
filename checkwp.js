3f16c2a9f8fc5dd9eacb65b535fde20bb7f106ba
Cesar <cesaroach@gmail.com>
2017-04-20 17:38:21 -0300

    Clean version

--- checkwp.js
+++ checkwp.js
@@ -1,17 +1,16 @@
-//this APP checks if an url has WordPress admin page
 const request = require('superagent');
 let URL = process.argv[2];
-//receive an URL and store it
-function storeUrl(val) {
+
+function checkUrl(val) {
 	let addwp = "/wp-admin";
 	request
-	.get(val + addwp)
-	.end(function(err, res) {
-		if (res.notFound === false) {
-			console.log('Has WordPress')
+		.get(val + addwp)
+		.end(function(err, res) {
+      if (res.notFound === false) {
+				console.log('Has WordPress');
 			} else {
-				console.log('The site does not have wp-admin')
+				console.log('The site does not have wp-admin');
 			}
 		});
 };
-storeUrl(URL);
+checkUrl(URL);
