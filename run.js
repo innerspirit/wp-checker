a596766760e290727701407f5c7748ef514ddc85
Micah Wood <micah@wpscholar.com>
2018-10-10 10:40:05 -0400

    Let the user know that a domain should be provided.

--- run.js
+++ run.js
@@ -2,6 +2,11 @@
 let checker = require('./checkwp.js');
 let domains = process.argv.slice(2);
 
+if (domains.length === 0) {
+    console.log('Please provide a domain.');
+    process.exit(1);
+}
+
 for (let i in domains) {
 	checker(domains[i]).then(logger.bind(domains[i]));
 }
